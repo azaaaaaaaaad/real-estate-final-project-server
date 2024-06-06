fa781a06a0912f1a3177b3b3e646a2f92fc00d31
jrazad10 <jrazad10@gmail.com>
2024-06-06 11:26:46 +0600

    jwt implemented

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const cors = require('cors');
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId, Timestamp } = require('mongodb');
 require('dotenv').config()
 const app = express();
+const cookieParser = require('cookie-parser')
+const jwt = require('jsonwebtoken')
+
 const port = process.env.PORT || 5000;
 
 //middleware
@@ -13,6 +16,23 @@ const corsOptions = {
 }
 app.use(cors(corsOptions));
 app.use(express.json())
+
+const verifyToken = async (req, res, next) => {
+    const token = req.cookies?.token
+    console.log(token)
+    if (!token) {
+        return res.status(401).send({ message: 'unauthorized access' })
+    }
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+            console.log(err)
+            return res.status(401).send({ message: 'unauthorized access' })
+        }
+        req.user = decoded
+        next()
+    })
+}
+
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.fdffxhb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
@@ -36,11 +56,48 @@ async function run() {
         const reviewsCollection = client.db("realEstateDB").collection("reviews")
         const usersCollection = client.db("realEstateDB").collection("users")
 
-
-        //save all user in db
-        app.post('/user', async(req,res)=> {
+        // auth related api
+        app.post('/jwt', async (req, res) => {
             const user = req.body
-            const result = await usersCollection.insertOne(user)
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+                expiresIn: '365d',
+            })
+            res
+                .cookie('token', token, {
+                    httpOnly: true,
+                    secure: process.env.NODE_ENV === 'production',
+                    sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
+                })
+                .send({ success: true })
+        })
+        // Logout
+        app.get('/logout', async (req, res) => {
+            try {
+                res
+                    .clearCookie('token', {
+                        maxAge: 0,
+                        secure: process.env.NODE_ENV === 'production',
+                        sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
+                    })
+                    .send({ success: true })
+                console.log('Logout successful')
+            } catch (err) {
+                res.status(500).send(err)
+            }
+        })
+
+        //save a user in db
+        app.put('/user', async (req, res) => {
+            const user = req.body
+            const options = { upsert: true }
+            const query = { email: user?.email }
+            const updateDoc = {
+                $set: {
+                    ...user,
+                    Timestamp: Date.now(),
+                },
+            }
+            const result = await usersCollection.updateOne(query, updateDoc, options)
             res.send(result)
         })
 
@@ -88,7 +145,7 @@ async function run() {
         //delete a property
         app.delete('/my-added-properties/:id', async (req, res) => {
             const id = req.params.id
-            const query = {_id: new ObjectId(id)}
+            const query = { _id: new ObjectId(id) }
             const result = await allPropertiesCollection.deleteOne(query)
             res.send(result)
         })
